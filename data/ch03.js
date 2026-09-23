ae4b6b8f26d4c133b5108215ac50d685a9839b5a
bbarmon26 <bilaschandrabarmon@gmail.com>
2026-09-23 16:25:25 +0600

    Create ch03.js

---
+++ data/ch03.js
@@ -0,0 +1,42 @@
+const chapterData = {
+  chapterId: 3,
+  chapterName: "০৩। নৌকা ও স্রোত",
+  questions: [
+    {
+      id: 1,
+      title: "সমস্যা (১)",
+      question: "স্রোতের অনুকূলে একটি নৌকা ৪ ঘণ্টায় ৩৬ কি.মি. পথ অতিক্রম করে। স্রোতের বেগ প্রতিঘণ্টায় ৩ কি.মি হলে, স্থির পানিতে নৌকার বেগ কত?",
+      solution: `
+        স্রোতের অনুকূলে নৌকাটি ৪ ঘণ্টায় যায় ৩৬ কি.মি.[span_0](start_span)[span_0](end_span)[span_1](start_span)[span_1](end_span)<br>
+        $\\therefore$ স্রোতের অনুকূলে নৌকাটি ১ ঘণ্টায় যায় = $\\frac{৩৬}{৪}$ কি.মি. = ৯ কি.মি.[span_2](start_span)[span_2](end_span)[span_3](start_span)[span_3](end_span)<br><br>
+        $\\therefore$ স্রোতের অনুকূলে বেগ = ৯ কি.মি./ঘণ্টা[span_4](start_span)[span_4](end_span)[span_5](start_span)[span_5](end_span)<br><br>
+        আমরা জানি,<br>
+        স্রোতের অনুকূলে, নৌকার গতিবেগ = নৌকার প্রকৃত গতিবেগ + স্রোতের গতিবেগ[span_6](start_span)[span_6](end_span)[span_7](start_span)[span_7](end_span)<br>
+        $\\therefore$ স্থির পানিতে নৌকার বেগ = স্রোতের অনুকূলে বেগ - স্রোতের বেগ[span_8](start_span)[span_8](end_span)[span_9](start_span)[span_9](end_span)<br>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;= $(৯ - ৩)$ কি.মি./ঘণ্টা[span_10](start_span)[span_10](end_span)[span_11](start_span)[span_11](end_span)<br>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;= ৬ কি.মি./ঘণ্টা[span_12](start_span)[span_12](end_span)[span_13](start_span)[span_13](end_span)<br><br>
+        <b>উত্তর:</b> ৬ কি.মি./ঘণ্টা।[span_14](start_span)[span_14](end_span)[span_15](start_span)[span_15](end_span)
+      `,
+      practice: [
+        "অনুরূপ: স্রোতের অনুকূলে একটি নৌকা ৪ ঘণ্টায় ৪০ কি.মি যায়। যদি স্থির জলে ঐ নৌকার গতিবেগ ঘণ্টায় ৮ কি.মি হয়, তবে নদীর স্রোতের গতিবেগ কত ছিল? [উত্তর: ২ কি.মি/ঘণ্টা][span_16](start_span)[span_17](start_span)"[span_16](end_span)[span_17](end_span)
+      ]
+    },
+    {
+      id: 2,
+      title: "সমস্যা (২)",
+      question: "স্রোতের প্রতিকূলে একটি জাহাজ ১১ ঘণ্টায় ৭৭ কি. মি. পথ অতিক্রম করে। স্থির পানিতে জাহাজের গতিবেগ প্রতিঘণ্টায় ৯ কি.মি. হলে, স্রোতের গতিবেগ প্রতিঘণ্টায় কত?",
+      solution: `
+        স্রোতের প্রতিকূলে জাহাজটি ১১ ঘণ্টায় যায় ৭৭ কি.মি.[span_18](start_span)[span_18](end_span)[span_19](start_span)[span_19](end_span)<br>
+        $\\therefore$ স্রোতের প্রতিকূলে জাহাজটি ১ ঘণ্টায় যায় = $\\frac{৭৭}{১১}$ কি.মি. = ৭ কি.মি.[span_20](start_span)[span_20](end_span)[span_21](start_span)[span_21](end_span)<br><br>
+        $\\therefore$ স্রোতের প্রতিকূলে গতিবেগ = ৭ কি.মি./ঘণ্টা[span_22](start_span)[span_22](end_span)[span_23](start_span)[span_23](end_span)<br><br>
+        আমরা জানি, স্রোতের প্রতিকূলে,<br>
+        জাহাজের গতিবেগ = জাহাজের প্রকৃত গতিবেগ - স্রোতের গতিবেগ[span_24](start_span)[span_24](end_span)[span_25](start_span)[span_25](end_span)<br>
+        $\\therefore$ স্রোতের গতিবেগ = স্থির পানিতে জাহাজের গতিবেগ - স্রোতের প্রতিকূলে গতিবেগ[span_26](start_span)[span_26](end_span)[span_27](start_span)[span_27](end_span)<br>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;= $(৯ - ৭)$ কি.মি./ঘণ্টা[span_28](start_span)[span_28](end_span)[span_29](start_span)[span_29](end_span)<br>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;= ২ কি.মি./ঘণ্টা[span_30](start_span)[span_30](end_span)[span_31](start_span)[span_31](end_span)<br><br>
+        <b>উত্তর:</b> স্রোতের গতিবেগ ২ কি.মি./ঘণ্টা।[span_32](start_span)[span_32](end_span)[span_33](start_span)[span_33](end_span)
+      `,
+      practice: [
+        "অনুরূপ: স্রোতের বিপরীতে একটি নৌকা ৫২ মিনিটে ১৩ কি.মি যেতে পারে। স্রোতের বেগ ৪ কি.মি/ঘণ্টা। স্থির পানিতে নৌকার বেগ কত? [উত্তর: ১৯ কি.মি/ঘণ্টা][span_34](start_span)[span_35](start_span)"[span_34](end_span)[span_35](end_span)
+      ]
+    }
